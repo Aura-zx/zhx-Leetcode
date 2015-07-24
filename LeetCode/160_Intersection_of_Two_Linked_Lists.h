d97c0534ccfa2c8887860cac739b274b7b2b5322
vision-zhx <kaomygod@gmail.com>
2015-07-24 21:55:38 +0800

    add #160

---
+++ LeetCode/160_Intersection_of_Two_Linked_Lists.h
@@ -0,0 +1,70 @@
+#ifndef INTERSECTION_OF_TWO_LINKED_LISTS_H
+#define	INTERSECTION_OF_TWO_LINKED_LISTS_H
+#include <iostream>
+
+class Solution_160
+{
+public:
+	struct ListNode
+	{
+		int val;
+		ListNode *next;
+		ListNode(int x) : val(x), next(NULL){}
+	};
+
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+	{
+		if (headA == NULL || headB == NULL)
+			return NULL;						// check if there exists empty list
+
+		ListNode *hA = headA;
+		ListNode *hB = headB;
+		int lengthA = 0;
+		int lengthB = 0;
+
+		while (headA != NULL)
+		{
+			headA = headA->next;
+			lengthA++;
+		}
+
+		while (headB != NULL)
+		{
+			headB = headB->next;
+			lengthB++;
+		}
+		
+		int max = lengthA > lengthB ? 1 : 0;
+		int diff = abs(lengthA - lengthB);
+		if (max == 1)
+		{
+			while (diff != 0)
+			{
+				hA = hA->next;
+				diff--;
+			}
+		}
+		else
+		{
+			while (diff != 0)
+			{
+				hB = hB->next;
+				diff--;
+			}
+		}
+
+		while (hA != NULL && hB != NULL)
+		{
+			if (hA == hB)
+				return hA;
+			else
+			{
+				hA = hA->next;
+				hB = hB->next;
+			}
+		}
+		return NULL;
+	}
+};
+
+#endif // !INTERSECTION_OF_TWO_LINKED_LISTS_H
--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,19 +2,9 @@
 //
 
 #include "stdafx.h"
-#include "35_Search_Insert_Position.h"
-#include "65_Valid_Number.h"
-#include "14_Longest_Common_Prefix.h"
-#include "217_Contains_Duplicate.h"
-#include "219_Contains_Duplicate_II.h"
-#include "58_Length_of_Last_Word.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Solution_58 sol = Solution_58( );
-	string s = "a  ";
-	cout << sol.lengthOfLastWord(s);
-	
 	return 0;
 }
 
