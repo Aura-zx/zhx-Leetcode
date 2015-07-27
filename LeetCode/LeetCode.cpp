0883ee9037ae9bf490ca3eee545cefac13e3f3a7
zhx <kaomygod@gmail.com>
2015-07-27 18:23:46 +0800

    #231

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,9 +2,16 @@
 //
 
 #include "stdafx.h"
+#include "231_Power_of_Two.h"
+
+#include <iostream>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Solution_231 s;
+	cout << s.isPowerOfTwo(5);
+		
 	return 0;
 }
 
