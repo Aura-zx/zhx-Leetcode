103e997444aec09b0edc685b27f907bbb0e4d51b
zhx <kaomygod@gmail.com>
2015-07-31 21:43:42 +0800

    #228

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,16 +2,18 @@
 //
 
 #include "stdafx.h"
-#include "9_Palindrome_Number.h"
+#include "228_Summary_Ranges.h"
 
 #include <iostream>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Solution_9 s;
-	cout << s.isPalindrome(10);
-		
+	Solution_228 s;
+	vector<int> n = {-1 };
+	vector<string> r = s.summaryRanges(n);
+
+	cout << "oo";
 	return 0;
 }
 
