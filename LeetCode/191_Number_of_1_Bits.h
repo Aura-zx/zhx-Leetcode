da4b4b5fcf2cadafa7409a9c3fdb4010996130c2
zhx <kaomygod@gmail.com>
2015-09-30 21:58:05 +0800

    #169

---
+++ LeetCode/191_Number_of_1_Bits.h
@@ -0,0 +1,12 @@
+#ifndef NUMBER_OF_1_BITS_H
+#define NUMBER_OF_1_BITS_H
+
+class Solution_191
+{
+public:	
+	int hammingWeight(uint32_t n)
+	{
+
+	}
+};
+#endif // !NUMBER_OF_1_BITS_H
--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,16 +2,17 @@
 //
 
 #include "stdafx.h"
-#include "283_Move_Zeros.h"
+#include "169_Majority_Element.h"
 
 #include <iostream>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Solution_283 s;
-	vector<int> v = { 1, 2, 4, 0, 6, 0, 6, 8, 32, 0, 0 };
-	s.moveZeroes(v);
+	Solution_169 s;
+	vector<int> v = { 1, 1, 2, 2, 3, 0, 6, 8, 32, 0, 0 };
+	int n = s.majorityElement(v);
+	cout << n << endl;
 	return 0;
 }
 
