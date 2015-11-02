c2359a5a29de10bbd6d1e8a9bf9940c6acd069b3
zhx <kaomygod@gmail.com>
2015-11-02 21:04:25 +0800

    #190 #36 #88

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,18 +2,27 @@
 //
 
 #include "stdafx.h"
-#include "172_Factorial_Trailing_Zeroes.h"
+#include "36_Valid_Sudoku.h"
 
 #include <iostream>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Solution_172 s = Solution_172( );
-	
-	s.trailingZeroes(5);
-
-
+	Solution_36 s = Solution_36( );
+	vector<vector<char>> t1 = 
+	{ 
+	{ '.', '8', '7', '6', '5', '4', '3', '2', '1'},
+	{ '2', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '3', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '4', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '5', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '6', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '7', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '8', '.', '.', '.', '.', '.', '.', '.', '.'},
+	{ '9', '.', '.', '.', '.', '.', '.', '.', '.'}, 
+	};
+	s.isValidSudoku(t1);
 	cout << "";
 	return 0;
 }
