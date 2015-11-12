f34a64b0e6224e5937b302ad0be753e7016614c9
zhx <kaomygod@gmail.com>
2015-11-12 20:18:40 +0800

    #303 #299

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,20 +2,20 @@
 //
 
 #include "stdafx.h"
-#include "7_Reverse_Integer.h"
+#include "299_Bulls_and_Cows.h"
 
 #include <iostream>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Solution_7 s = Solution_7( );
-	string a("1010");
-	string b("1011");
+	Solution_299 s = Solution_299( );
+	string a("1122");
+	string b("1222");
 
 	
-	cout << s.reverse(123);
-	cout << s.reverse(-123);
+	cout << s.getHint(a, b);
+
 	return 0;
 }
 
