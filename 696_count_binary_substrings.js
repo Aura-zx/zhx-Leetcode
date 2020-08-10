c15ec4d904d0e5f9bffcd001f90a0cbceaeb659c
Aura-zx <kaomygod@gmail.com>
2020-08-10 22:56:26 +0800

    feat: 696

---
+++ 696_count_binary_substrings.js
@@ -0,0 +1,33 @@
+// Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+// Substrings that occur multiple times are counted the number of times they occur.
+
+// Example 1:
+// Input: "00110011"
+// Output: 6
+// Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+
+// Notice that some of these substrings repeat and are counted the number of times they occur.
+
+// Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+
+var countBinarySubstrings = function(s) {
+	let ans = 0;
+
+	let prev = 0;
+	let cur = 1;
+	for (let i = 0; i < s.length - 1; i++) {
+		if (s[i] === s[i + 1]) {
+			cur++;
+		} else {
+			prev = cur;
+			cur = 0;
+			cur++;
+		}
+		if (prev >= cur) {
+			ans++;
+		}
+	}
+
+	return ans;
+};
